2750b95772a93f5e768c4d4b2cb1de4e2c62042b
Fabio-Ricci <fabioricci9@gmail.com>
2019-09-18 15:12:00 -0300

    plotting histogram

--- trab2/main.py
+++ trab2/main.py
@@ -2,9 +2,17 @@ import cv2
 import sys
 import os
 import methods
+from matplotlib import pyplot as plt
 
 def readImage(filepath):
     return cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+
+def plotHistogram(img, title):
+    plt.title(title)
+    plt.xlabel('Valor do pixel')
+    plt.ylabel('Quantidade')
+    plt.hist(img)
+    plt.show()
 
 if __name__ == '__main__':
     in_file = sys.argv[1]
@@ -16,25 +24,26 @@ if __name__ == '__main__':
         os.makedirs("./out/")
 
     img = methods._global(image)
-    cv2.imwrite(out_path + "_global.png", img)
+    plotHistogram(img, "Global Method")
+    # cv2.imwrite(out_path + "_global.png", img)
 
-    img = methods._bernsen(image)
-    cv2.imwrite(out_path + "_bernsen.png", img)
+    # img = methods._bernsen(image)
+    # cv2.imwrite(out_path + "_bernsen.png", img)
 
-    img = methods._niblack(image)
-    cv2.imwrite(out_path + "_niblack.png", img)
+    # img = methods._niblack(image)
+    # cv2.imwrite(out_path + "_niblack.png", img)
 
-    img = methods._sauvola_pietaksinen(image)
-    cv2.imwrite(out_path + "_sauvola_pietaksinen.png", img)
+    # img = methods._sauvola_pietaksinen(image)
+    # cv2.imwrite(out_path + "_sauvola_pietaksinen.png", img)
 
-    img = methods._phansalskar(image)
-    cv2.imwrite(out_path + "_phansalskar.png", img)
+    # img = methods._phansalskar(image)
+    # cv2.imwrite(out_path + "_phansalskar.png", img)
 
-    img = methods._contrast(image)
-    cv2.imwrite(out_path + "_contrast.png", img)
+    # img = methods._contrast(image)
+    # cv2.imwrite(out_path + "_contrast.png", img)
 
-    img = methods._average(image)
-    cv2.imwrite(out_path + "_average.png", img)
+    # img = methods._average(image)
+    # cv2.imwrite(out_path + "_average.png", img)
 
-    img = methods._median(image)
-    cv2.imwrite(out_path + "_median.png", img)
+    # img = methods._median(image)
+    # cv2.imwrite(out_path + "_median.png", img)
