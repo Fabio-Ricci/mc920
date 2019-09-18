4806a7901a203087b9b8510ba79186ab72a1b40d
Fabio-Ricci <fabioricci9@gmail.com>
2019-09-17 23:39:00 -0300

    bernsen method

--- trab2/main.py
+++ trab2/main.py
@@ -11,6 +11,12 @@ if __name__ == '__main__':
     out_file = sys.argv[2]
     out_path = os.path.dirname(os.path.realpath(__file__)) + "/out/" + out_file
     image = readImage(in_file)
-    
-    img = methods.globalMethod(image)
+
+    if not os.path.exists("./out/"):
+        os.makedirs("./out/")
+
+    img = methods._global(image)
     cv2.imwrite(out_path + "_global.png", img)
+
+    img = methods._bernsen(image)
+    cv2.imwrite(out_path + "_bernsen.png", img)
--- trab2/methods.py
+++ trab2/methods.py
@@ -1,14 +1,19 @@
 import numpy as np
 
-def max_in_neighborhood(size):
-    delta = (size - 1) / 2
-    [x-delta:x+delta+1] #[A, B)
-
-def globalMethod(image):
+def _global(image):
     img = np.copy(image)
     img = np.where(img < 128, 0, 255)
     return img
 
-def bernsenMethod(image):
+def min_max_in_neighborhood(image, delta, x, y):
+    m = image[x-delta:x+delta+1, y-delta:y+delta+1]
+    return (m.min(), m.max())
+    
+def _bernsen(image):
     img = np.copy(image)
+    delta = 1
+    for x in range(delta, img.shape[0]-delta):
+        for y in range(delta, img.shape[1]-delta):
+            (min, max) = min_max_in_neighborhood(image, delta, x, y)
+            img[x][y] = (int(min) + int(max))/2
     return img
