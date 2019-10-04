27e8f5bb36e27b46c80436a2a7c2773e64a2450a
Fabio-Ricci <fabioricci9@gmail.com>
2019-10-04 14:49:21 -0300

    implemented trab3 methods

--- trab3/main.py
+++ trab3/main.py
@@ -7,15 +7,8 @@ from matplotlib import pyplot as plt
 def readImage(filepath):
     return cv2.imread(filepath, cv2.IMREAD_COLOR)
 
-def printImage(filepath, image):
+def saveImage(filepath, image):
     cv2.imwrite(filepath, image)
-
-def saveHistogram(image, title, path, filename):
-    plt.title(title)
-    plt.xlabel('Nível de cinza')
-    plt.ylabel('Quantidade de pixels')
-    plt.hist(image, bins=255)
-    plt.savefig(path + filename + "_histogram")
 
 def convertColors(image):
     img = np.copy(image)
@@ -24,6 +17,57 @@ def convertColors(image):
         for y in range(image.shape[1]):
             img[x][y] = (int(image_r[x][y]) + int(image_g[x][y]) + int(image_b[x][y]))/3
     return img
+
+def getEdges(image):
+    img = cv2.Canny(image,100,200)
+    return np.where(img == 0, 255, 0)
+
+def getCentroid(image):  
+    img = np.copy(image)
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
+    ret, thresh = cv2.threshold(gray_image,127,255,0)
+    contours, hierarchy = cv2.findContours(thresh, 1, 2)
+    i=0
+    print("Número de regiões: ", len(contours), "\n") 
+    areas = np.zeros(len(contours))
+    for c in contours:
+        # calculate moments for each contour
+        M = cv2.moments(c)
+        
+        # calculate x,y coordinate of center
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+        cv2.putText(img, str(i), (cX-4, cY+4), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
+        # Area
+        areas[i] = cv2.contourArea(c)
+        # Perimeter
+        perimeter = round(cv2.arcLength(c, True), 6)
+        # Eccentricity
+        a1 = (M['mu20']+M['mu02'])/2
+        a2 = np.sqrt(4*M['mu11']**2+(M['mu20']-M['mu02'])**2)/2
+        minor_axis = a1-a2
+        major_axis = a1+a2
+        eccentricity = round(np.sqrt(1-minor_axis/major_axis), 6)
+        # Solidity
+        hull = cv2.convexHull(c)
+        hull_area = cv2.contourArea(hull)
+        solidity = round(float(areas[i])/hull_area, 6)
+
+        print("Região", str(i) + ":", "área:", areas[i], "perímetro:", perimeter, "excentricidade:", eccentricity, "solidez:", solidity)
+        i = i + 1
+    return (img, areas)
+
+def getAreasHistogram(areas, filepath):
+    areasNumber = np.zeros(3)
+    areasNumber[0] = len(areas[areas < 1500])
+    areasNumber[1] = len([a for a in areas if a >= 1500 and a < 3000])
+    areasNumber[2] = len(areas[areas >= 3000])
+    plt.title('Histograma de áreas dos objetos')
+    plt.xlabel('Area')
+    plt.ylabel('Número de Objetos')
+    plt.hist(areas, bins=3)
+    plt.savefig(filepath)
 
 if __name__ == '__main__':
     in_file = sys.argv[1]
@@ -35,5 +79,14 @@ if __name__ == '__main__':
         os.makedirs(out_path)
 
     image = readImage(in_file)
+
     img = convertColors(image)
-    print(img)
+    saveImage(out_path + out_file + "_1.png", img)
+
+    img = getEdges(image)
+    saveImage(out_path + out_file + "_2.png", img)
+
+    (img, areas) = getCentroid(image)
+    saveImage(out_path + out_file + "_3.png", img)
+
+    getAreasHistogram(areas, out_path + out_file + "_4.png")
