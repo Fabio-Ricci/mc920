7f27f5704638256aaa71cf534adf365267b26043
Fabio-Ricci <fabioricci9@gmail.com>
2019-10-20 23:12:29 -0300

    deconding working

--- trab4/codificar.py
+++ trab4/codificar.py
@@ -15,19 +15,44 @@ def readText(in_text):
     text_file.close()
     return text
 
+def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
+    bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
+
+def replaceBits(num, bit, plano_bits):
+    mask = 1 << plano_bits
+    return (num & ~mask) | ((bit << plano_bits) & mask)
+
 def writeTextOnImage(image_r, image_g, image_b, text, plano_bits):
     width = image_r.shape[0]
     height = image_r.shape[1]
-    # for i in range(0, width):
-    #     for i in range(0, height):
-
+    text_size = len(text)
+    text = bin(text_size)[2:] + text
+    i = 0
+    for x in range(width):
+        for y in range(height):
+            if i == text_size:
+                break
+            image_r[x][y] = replaceBits(image_r[x][y], int(text[i]), plano_bits)
+            i += 1
+            if i == text_size:
+                break
+            image_g[x][y] = replaceBits(image_g[x][y], int(text[i]), plano_bits)
+            i += 1
+            if i == text_size:
+                break
+            image_b[x][y] = replaceBits(image_b[x][y], int(text[i]), plano_bits)
+            i += 1
+        if i == text_size:
+            break
     return (image_r, image_g, image_b)
 
 if __name__ == '__main__':
     in_file = sys.argv[1]
     in_text = sys.argv[2]
-    plano_bits = sys.argv[3]
+    plano_bits = int(sys.argv[3])
     out_file = sys.argv[4]
+    out_file = out_file[:-4]
     out_path = os.path.dirname(os.path.realpath(__file__)) + "/out/" + out_file + "/"
     if not os.path.exists("./out/"):
         os.makedirs("./out/")
@@ -38,8 +63,9 @@ if __name__ == '__main__':
     image = readImage(in_file)
     (image_r, image_g, image_b) = cv2.split(image)
 
-    # Read text and convert to ASCII
-    text = [ord(c) for c in readText(in_text)]
+    # Read text and convert to binary
+    text = readText(in_text)
+    text = text_to_bits(text)
 
     # Write text on image
     (image_r, image_g, image_b) = writeTextOnImage(image_r, image_g, image_b, text, plano_bits)
--- trab4/decodificar.py
+++ trab4/decodificar.py
@@ -2,6 +2,9 @@ import cv2
 import sys
 import os
 import numpy as np
+import binascii
+
+MAX_TEXT_SIZE = 8
 
 def readImage(filepath):
     return cv2.imread(filepath, cv2.IMREAD_COLOR)
@@ -15,20 +18,73 @@ def readText(in_text):
     text_file.close()
     return text
 
-def readTextOnImage(image, text):
-    return text
+def saveText(filepath, text):
+    f = open(filepath, "w+")
+    f.write(text)
+    f.close()
+
+def readBit(num, plano_bits):
+    return num >> plano_bits & 1
+
+def decode_binary_string(s):
+    return ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
+
+def readTextOnImage(image_r, image_g, image_b, plano_bits):
+    width = image_r.shape[0]
+    height = image_r.shape[1]
+    text_size = ""
+    text = ""
+    i = 0
+    x = 0
+    y = 0
+    for x in range(width):
+        for y in range(height):
+            if i == MAX_TEXT_SIZE:
+                break
+            text_size += str(readBit(image_r[x][y], plano_bits))
+            i += 1
+            if i == MAX_TEXT_SIZE:
+                break
+            text_size += str(readBit(image_g[x][y], plano_bits))
+            i += 1
+            if i == MAX_TEXT_SIZE:
+                break
+            text_size += str(readBit(image_b[x][y], plano_bits))
+            i += 1
+        if i == MAX_TEXT_SIZE:
+            break    
+    text_size = decode_binary_string(text_size)
+    for x in range(x, width):
+        for y in range(y, height):
+            if i == text_size:
+                break
+            text += str(readBit(image_r[x][y], plano_bits))
+            i += 1
+            if i == text_size:
+                break
+            text += str(readBit(image_g[x][y], plano_bits))
+            i += 1
+            if i == text_size:
+                break
+            text += str(readBit(image_b[x][y], plano_bits))
+            i += 1
+        if i == text_size:
+            break
+    return decode_binary_string(text[:30])
 
 if __name__ == '__main__':
     in_file = sys.argv[1]
-    plano_bits = sys.argv[2]
+    image_name = in_file[:-4]
+    plano_bits = int(sys.argv[2])
     out_file = sys.argv[3]
-    out_path = os.path.dirname(os.path.realpath(__file__)) + "/out/" + out_file + "/"
-    if not os.path.exists("./out/"):
-        os.makedirs("./out/")
-    if not os.path.exists(out_path):
-        os.makedirs(out_path)
 
+    # Read the coded image
     image = readImage(in_file)
     (image_r, image_g, image_b) = cv2.split(image)
 
-    text = readTextOnImage(image)
+    # Extract text from image
+    text = readTextOnImage(image_r, image_g, image_b, plano_bits)
+    print(text)
+
+    # Save the text on file
+    saveText(out_file, text)
